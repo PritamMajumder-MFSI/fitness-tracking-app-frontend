e792cc809fb98a9958268c7f69c722ef4b2ebe7f
PritamMajumder-MFSI <pritamm@mindfiresolutions.com>
2024-10-25 19:19:02 +0530

    Auth layout and login component

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { AuthLayoutComponent } from '../../../components/layout/auth-layout/auth-layout.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [AuthLayoutComponent],
+  imports: [
+    AuthLayoutComponent,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
